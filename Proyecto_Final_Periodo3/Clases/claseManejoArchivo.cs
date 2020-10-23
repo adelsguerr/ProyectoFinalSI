292689ec18ed60a0ebebc21a619c70b1149fd31c
Adelso <adelso.guerra@outlook.com>
2020-10-23 16:33:21 -0600

    Revert "He creado un comentario"
    This reverts commit ae6a5eb1da1249179666be0f32f61fb2bd6ce900.

--- Proyecto_Final_Periodo3/Clases/claseManejoArchivo.cs
+++ Proyecto_Final_Periodo3/Clases/claseManejoArchivo.cs
@@ -7,8 +7,6 @@ using System.Windows.Forms;
 using System.IO;
 using System.Runtime.Serialization.Formatters.Binary;
 
-//Adelso Guerra
-
 namespace Proyecto_Final_Periodo3.Clases
 {
     class claseManejoArchivo
