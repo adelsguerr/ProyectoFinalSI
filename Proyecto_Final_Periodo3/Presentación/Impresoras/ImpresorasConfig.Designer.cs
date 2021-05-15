1dd43ac5fdd88d1f8bbe73254fced8163d4e8445
AdelsGuerr24 <adelso.guerra@outlook.com>
2021-05-14 18:34:17 -0600

    cambios

---
+++ Proyecto_Final_Periodo3/Presentación/Impresoras/ImpresorasConfig.Designer.cs
@@ -0,0 +1,80 @@
+﻿
+namespace Proyecto_Final_Periodo3.Presentación.Impresoras
+{
+    partial class ImpresorasConfig
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblImpresoras = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblImpresoras
+            // 
+            this.lblImpresoras.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblImpresoras.AutoSize = true;
+            this.lblImpresoras.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblImpresoras.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.lblImpresoras.Location = new System.Drawing.Point(339, 20);
+            this.lblImpresoras.Name = "lblImpresoras";
+            this.lblImpresoras.Size = new System.Drawing.Size(119, 25);
+            this.lblImpresoras.TabIndex = 0;
+            this.lblImpresoras.Text = "Impresoras";
+            this.lblImpresoras.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(31)))), ((int)(((byte)(31)))), ((int)(((byte)(31)))));
+            this.panel1.Controls.Add(this.lblImpresoras);
+            this.panel1.Location = new System.Drawing.Point(1, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(799, 62);
+            this.panel1.TabIndex = 0;
+            // 
+            // ImpresorasConfig
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.Name = "ImpresorasConfig";
+            this.Text = "ImpresorasConfig";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblImpresoras;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
